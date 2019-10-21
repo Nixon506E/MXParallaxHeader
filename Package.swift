42198ba274d89c17f02d74dffc76acd705f39695
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2019-10-21 13:47:42 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .target(
             name: "MXParallaxHeader",
             path: "MXParallaxHeader",
-            publicHeadersPath: "MXParallaxHeader"
+            publicHeadersPath: "Framework"
         )
     ],
     cLanguageStandard: .c11
